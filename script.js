fe539c462ae784fbd5950180c95e6fbcef945644
ThiagoEufrauso <tcunha304@gmail.com>
2024-03-20 10:32:52 -0300

    eu sou o pika

--- script.js
+++ script.js
@@ -1,26 +1,3 @@
-
-//let a = "5";
-//let b = 2;
-
-//console.log(typeof(a));
-//console.log(typeof(b));
-
-
-//let numero1 = 0.1;
-//let numero2 = 1;
-
-//console.log(numero1 + numero2);
-
-let texto1 = "ola";
-let texto2 = "mundo";
-
-let resultadotexto = texto1 + "," + texto2;
-
-console.log(resultadotexto)
-
-//let resultado = a - b;
-
-//let resultado2 = a + b;
-
-//console.log(resultado)
-//console.log(resultado2)
+for(let i = 1; i < 100; i += 5) {
+    console.log(i);
+}
