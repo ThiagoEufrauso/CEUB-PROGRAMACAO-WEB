19aaf47ca4bc3e39d5a5934fd992d9a662f5be09
ThiagoEufrauso <tcunha304@gmail.com>
2024-03-20 09:05:02 -0300

    Alterações 20/03 javascript

---
+++ script.js
@@ -0,0 +1,26 @@
+
+//let a = "5";
+//let b = 2;
+
+//console.log(typeof(a));
+//console.log(typeof(b));
+
+
+//let numero1 = 0.1;
+//let numero2 = 1;
+
+//console.log(numero1 + numero2);
+
+let texto1 = "ola";
+let texto2 = "mundo";
+
+let resultadotexto = texto1 + "," + texto2;
+
+console.log(resultadotexto)
+
+//let resultado = a - b;
+
+//let resultado2 = a + b;
+
+//console.log(resultado)
+//console.log(resultado2)
